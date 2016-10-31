f160a6c95214691e7872657fb7d46c07d47d8e58
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2016-10-31 15:46:33 -0700

    baseline 0.1

--- customize.sh
+++ customize.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 module=$1
-for file in `grep -Ril  *`; do 
+for file in `grep -Ril  * | grep -v customize.sh`; do 
   echo "renaming  to $module in $file"
   sed -i"bak" "s/qube_base/$module/g" $file;
   rm -rf $file.bak
 done
 
 if [ -d  ]; then
-  echo "renaming director  to $module"
+  echo "renaming directory qube_base  to $module"
   mv  $module
 fi
