a2fab0c88ec5686cac16b4f86a95e2cb9c956008
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2016-10-31 23:10:15 -0700

    baseline 0.6

--- customize.sh
+++ customize.sh
@@ -3,17 +3,17 @@ module=$1
 repo=$2
 sonar_key=$3
 
-for file in `grep -Ril qube_placeholder * | grep -v customize.sh`; do 
+for file in `grep -Ril qube_placeholder * | grep -v customize.sh | grep -v .whl`; do 
   echo "renaming  qube_placeholder to $module in $file"
-  sed -i".bak" "s/qube_base/$module/g" $file;
+  sed -i".bak" "s/qube_placeholder/$module/g" $file;
   rm -rf $file.*bak*
 done
-for file in `grep -Ril QUBE_SONAR_KEY * | grep -v customize.sh`; do 
+for file in `grep -Ril QUBE_SONAR_KEY * | grep -v customize.sh| grep -v .whl`; do 
   echo "renaming  QUBE_SONAR_KEY to $sonar_key in $file"
   sed -i".bak" "s/QUBE_SONAR_KEY/$sonar_key/g" $file;
   rm -rf $file.*bak*
 done
-for file in `grep -Ril qube_repo * | grep -v customize.sh`; do 
+for file in `grep -Ril qube_repo * | grep -v customize.sh| grep -v .whl`; do 
   echo "renaming  qube_repo to $repo in $file"
   sed -i".bak" "s/qube_repo/$repo/g" $file;
   rm -rf $file.*bak*
