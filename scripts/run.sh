268424fa700722349a3144c20f1dc8670ff7aad4
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2016-11-02 11:58:17 -0700

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 docker rm -f qube_placeholder_1
 
-docker run --name qube_placeholder_1 -d -it -p 5000:$DEFAULT_LISTENER_PORT \
+docker run --name qube_placeholder_1 -d -it -p $DEFAULT_LISTENER_PORT:$DEFAULT_LISTENER_PORT \
   -e DEFAULT_LISTENER_PORT=$DEFAULT_LISTENER_PORT \
   -e DEFAULT_LISTENER_HOST=$DEFAULT_LISTENER_HOST \
   qube_placeholder
