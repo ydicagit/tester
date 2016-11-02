aaee57c509074095ab77e6ea6c069d2ce32cdff0
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2016-11-02 12:49:00 -0700

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
+if [ -e .env.sh ]; then
+ source .env.sh
+fi
 docker rm -f qube_placeholder_1
 
 docker run --name qube_placeholder_1 -d -it -p $DEFAULT_LISTENER_PORT:$DEFAULT_LISTENER_PORT \
