ba89da65c16ad68191490250a50027188664ebdc
Hyunji Kim <Hyunji.Kim@ca.com>
2016-11-03 22:36:37 -0700

    IMG_NAME

--- scripts/bake.sh
+++ scripts/bake.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
-docker build -t qube_placeholder .
+
+docker build -t $QUBE_DOCKER_IMAGE_LOCAL .
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
+
 if [ -e .env.sh ]; then
  source .env.sh
 fi
+
+IMG_NAME=$QUBE_DOCKER_IMAGE_LOCAL
+
+if [ "$RUN_FROM_REMOTE_REGISTRY" == "1" ]; then
+   echo "repository pulled from gcloud gcr"
+   gcloud docker pull $QUBE_DOCKER_IMAGE
+   IMG_NAME=$QUBE_DOCKER_IMAGE
+fi
+
 docker rm -f qube_placeholder_1
 
 docker run --name qube_placeholder_1 -d -it -p $DEFAULT_LISTENER_PORT:$DEFAULT_LISTENER_PORT \
   -e DEFAULT_LISTENER_PORT=$DEFAULT_LISTENER_PORT \
   -e DEFAULT_LISTENER_HOST=$DEFAULT_LISTENER_HOST \
-  qube_placeholder
+  $IMG_NAME
